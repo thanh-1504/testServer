ded1fb80441825e61b48bb6a6bdad0dc565fc6a3
thanh-1504 <114299189+thanh-1504@users.noreply.github.com>
2025-05-14 22:21:19 +0700

    Update home.js

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,5 +1,5 @@
-const express =require("express")
-const router =express.Router()
-const controller = require("../controllers/tim-goiyController")
-router.get('/',controller.timKiem_goiy)
-module.exports= router;
+// const express =require("express")
+// const router =express.Router()
+// const controller = require("../controllers/tim-goiyController")
+// router.get('/',controller.timKiem_goiy)
+// module.exports= router;
